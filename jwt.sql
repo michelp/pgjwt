67fcabcfa7a3d7f6d529afede1aca6b4b2e4a739
Michel Pelletier <pelletier.michel@gmail.com>
2016-04-24 14:30:47 -0700

    works!

--- jwt.sql
+++ jwt.sql
@@ -3,31 +3,29 @@ DROP SCHEMA IF EXISTS jwt CASCADE;
 CREATE SCHEMA jwt;
 
 
-CREATE OR REPLACE FUNCTION jwt.base64_urlencode(data bytea) RETURNS text LANGUAGE sql AS $$
+CREATE OR REPLACE FUNCTION jwt.encode(data bytea) RETURNS text LANGUAGE sql AS $$
     SELECT translate(encode(data, 'base64'), '+/=', '-_');
 $$;
 
-CREATE OR REPLACE FUNCTION jwt.base64_urldecode(data text) RETURNS text LANGUAGE sql AS $$
-    SELECT convert_from(decode(translate(data, '-_', '+/') || '=', 'base64'), 'utf8')
+CREATE OR REPLACE FUNCTION jwt.decode(data text) RETURNS bytea LANGUAGE sql AS $$
+    SELECT decode(translate(data, '-_', '+/') || '=', 'base64');
 $$;
 
 
-CREATE OR REPLACE FUNCTION jwt.sign(data text, secret text, algorithm text DEFAULT 'sha256') 
+CREATE OR REPLACE FUNCTION jwt.sign(data text, secret text, algorithm text DEFAULT 'sha256')
 RETURNS text LANGUAGE sql AS $$
-SELECT jwt.base64_urlencode(hmac(data, secret, algorithm));
+SELECT jwt.encode(hmac(data, secret, algorithm));
 $$;
 
-CREATE OR REPLACE FUNCTION jwt.encode(payload json, secret text, algorithm text DEFAULT 'sha256') 
+CREATE OR REPLACE FUNCTION jwt.jwt(payload json, secret text, algorithm text DEFAULT 'sha256')
 RETURNS text LANGUAGE sql AS $$
-WITH header AS (SELECT jwt.base64_urlencode('{"alg": "HS256", "typ": "JWT"}')),
-     payload AS (SELECT jwt.base64_urlencode(convert_to(payload::text, 'utf8')))
-SELECT (SELECT * FROM header) 
-    || '.' || 
-    (SELECT * FROM payload) 
-    || '.' || 
-    jwt.sign((SELECT * FROM header) || '.' || (SELECT * FROM payload), 
-             secret, algorithm);
+WITH header AS (SELECT jwt.encode(convert_to('{"alg":"HS256","typ":"JWT"}', 'utf8'))),
+     payload AS (SELECT jwt.encode(convert_to(payload::text, 'utf8'))),
+     signables AS (SELECT (SELECT * FROM header) || '.' || (SELECT * FROM payload))
+SELECT
+    (SELECT * FROM signables)
+    || '.' ||
+    jwt.sign((SELECT * FROM signables), secret, algorithm);
 $$;
 
 COMMIT;
-
