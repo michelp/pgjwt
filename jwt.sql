028fb487486bd1f219d3793f431de6eb5fb866af
Michel Pelletier <pelletier.michel@gmail.com>
2016-04-24 23:00:23 -0700

    cleanup

--- jwt.sql
+++ jwt.sql
@@ -12,10 +12,10 @@ CREATE OR REPLACE FUNCTION jwt.url_decode(data text) RETURNS bytea LANGUAGE sql 
 WITH t AS (SELECT translate(data, '-_', '+/')),
      rem AS (SELECT length((SELECT * FROM t)) % 4) -- compute padding size
     SELECT decode(
-        (SELECT * FROM t) || 
-        CASE WHEN (SELECT * FROM rem) > 0 
-           THEN repeat('=', (4 - (SELECT * FROM rem))) 
-           ELSE '' END, 
+        (SELECT * FROM t) ||
+        CASE WHEN (SELECT * FROM rem) > 0
+           THEN repeat('=', (4 - (SELECT * FROM rem)))
+           ELSE '' END,
     'base64');
 $$;
 
