36bdc6c6babeef1d6966567773cc38bd3bd8539b
Michel Pelletier <pelletier.michel@gmail.com>
2016-04-25 11:43:55 -0700

    HS385 and HS512

--- jwt.sql
+++ jwt.sql
@@ -20,23 +20,42 @@ WITH t AS (SELECT translate(data, '-_', '+/')),
 $$;
 
 
-CREATE OR REPLACE FUNCTION jwt.sign(payload json, secret text, algorithm text DEFAULT 'sha256')
+CREATE OR REPLACE FUNCTION jwt.algorithm_sign(signables text, secret text, algorithm text)
 RETURNS text LANGUAGE sql AS $$
-WITH header AS (SELECT jwt.url_encode(convert_to('{"alg":"HS256","typ":"JWT"}', 'utf8'))),
-     payload AS (SELECT jwt.url_encode(convert_to(payload::text, 'utf8'))),
-     signables AS (SELECT (SELECT * FROM header) || '.' || (SELECT * FROM payload))
+WITH
+  alg AS (
+    SELECT CASE
+      WHEN algorithm = 'HS256' THEN 'sha256'
+      WHEN algorithm = 'HS384' THEN 'sha384'
+      WHEN algorithm = 'HS512' THEN 'sha512'
+      ELSE '' END)  -- throws error
+SELECT jwt.url_encode(hmac(signables, secret, (select * from alg)));
+$$;
+
+CREATE OR REPLACE FUNCTION jwt.sign(payload json, secret text, algorithm text DEFAULT 'HS256')
+RETURNS text LANGUAGE sql AS $$
+WITH
+  header AS (
+    SELECT jwt.url_encode(convert_to('{"alg":"' || algorithm || '","typ":"JWT"}', 'utf8'))
+    ),
+  payload AS (
+    SELECT jwt.url_encode(convert_to(payload::text, 'utf8'))
+    ),
+  signables AS (
+    SELECT (SELECT * FROM header) || '.' || (SELECT * FROM payload)
+    )
 SELECT
     (SELECT * FROM signables)
     || '.' ||
-    jwt.url_encode(hmac((SELECT * FROM signables), secret, algorithm));
+    jwt.algorithm_sign((SELECT * FROM signables), secret, algorithm);
 $$;
 
 
-CREATE OR REPLACE FUNCTION jwt.verify(token text, secret text, algorithm text DEFAULT 'sha256')
+CREATE OR REPLACE FUNCTION jwt.verify(token text, secret text, algorithm text DEFAULT 'HS256')
 RETURNS table(header json, payload json, valid boolean) LANGUAGE sql AS $$
-    SELECT convert_from(jwt.url_decode(r[1]), 'utf8')::json as header,
-           convert_from(jwt.url_decode(r[2]), 'utf8')::json as payload,
-           r[3] = jwt.url_encode(hmac(r[1] || '.' || r[2], secret, algorithm)) as valid
-    FROM regexp_split_to_array(token, '\.') r;
+  SELECT convert_from(jwt.url_decode(r[1]), 'utf8')::json as header,
+    convert_from(jwt.url_decode(r[2]), 'utf8')::json as payload,
+    r[3] = jwt.algorithm_sign(r[1] || '.' || r[2], secret, algorithm) as valid
+  FROM regexp_split_to_array(token, '\.') r;
 $$;
 COMMIT;
