f3d82fd30151e754e19ce5d6a06c71c20689ce3d
Michel Pelletier <michel@supabase.io>
2023-03-02 11:32:22 -0800

    missing some functions in 0.2.0 file for tle compat.

--- pgjwt--0.2.0.sql
+++ pgjwt--0.2.0.sql
@@ -1,3 +1,54 @@
+\echo Use "CREATE EXTENSION pgjwt" to load this file. \quit
+
+
+CREATE OR REPLACE FUNCTION url_encode(data bytea) RETURNS text LANGUAGE sql AS $$
+    SELECT translate(encode(data, 'base64'), E'+/=\n', '-_');
+$$ IMMUTABLE;
+
+
+CREATE OR REPLACE FUNCTION url_decode(data text) RETURNS bytea LANGUAGE sql AS $$
+WITH t AS (SELECT translate(data, '-_', '+/') AS trans),
+     rem AS (SELECT length(t.trans) % 4 AS remainder FROM t) -- compute padding size
+    SELECT decode(
+        t.trans ||
+        CASE WHEN rem.remainder > 0
+           THEN repeat('=', (4 - rem.remainder))
+           ELSE '' END,
+    'base64') FROM t, rem;
+$$ IMMUTABLE;
+
+
+CREATE OR REPLACE FUNCTION algorithm_sign(signables text, secret text, algorithm text)
+RETURNS text LANGUAGE sql AS $$
+WITH
+  alg AS (
+    SELECT CASE
+      WHEN algorithm = 'HS256' THEN 'sha256'
+      WHEN algorithm = 'HS384' THEN 'sha384'
+      WHEN algorithm = 'HS512' THEN 'sha512'
+      ELSE '' END AS id)  -- hmac throws error
+SELECT @extschema@.url_encode(@extschema@.hmac(signables, secret, alg.id)) FROM alg;
+$$ IMMUTABLE;
+
+
+CREATE OR REPLACE FUNCTION sign(payload json, secret text, algorithm text DEFAULT 'HS256')
+RETURNS text LANGUAGE sql AS $$
+WITH
+  header AS (
+    SELECT @extschema@.url_encode(convert_to('{"alg":"' || algorithm || '","typ":"JWT"}', 'utf8')) AS data
+    ),
+  payload AS (
+    SELECT @extschema@.url_encode(convert_to(payload::text, 'utf8')) AS data
+    ),
+  signables AS (
+    SELECT header.data || '.' || payload.data AS data FROM header, payload
+    )
+SELECT
+    signables.data || '.' ||
+    @extschema@.algorithm_sign(signables.data, secret, algorithm) FROM signables;
+$$ IMMUTABLE;
+
+
 CREATE OR REPLACE FUNCTION try_cast_double(inp text)
 RETURNS double precision AS $$
   BEGIN
